929e8728843020dc66654c8ac085917bef092f78
Chuks Grinage <chuksgrinage@gmail.com>
2019-10-13 15:05:11 -0500

    Updates

---
+++ precache-manifest.2f74c2f049686f13fe513da3adbbf3be.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9396d3f0f4d4ea06fae1776f3335df4d",
+    "url": "/UpliftConference/index.html"
+  },
+  {
+    "revision": "97cb4e3ded06f98532f7",
+    "url": "/UpliftConference/static/css/2.5bc09813.chunk.css"
+  },
+  {
+    "revision": "e73a964d14c04b48c578",
+    "url": "/UpliftConference/static/css/main.869aa8e3.chunk.css"
+  },
+  {
+    "revision": "97cb4e3ded06f98532f7",
+    "url": "/UpliftConference/static/js/2.ea4c0c8a.chunk.js"
+  },
+  {
+    "revision": "e73a964d14c04b48c578",
+    "url": "/UpliftConference/static/js/main.ba703b2c.chunk.js"
+  },
+  {
+    "revision": "ce182363eaafeae8ee75",
+    "url": "/UpliftConference/static/js/runtime-main.6b97e277.js"
+  },
+  {
+    "revision": "6f2c254c84b384fef1063a137adc55ad",
+    "url": "/UpliftConference/static/media/facebook2.6f2c254c.svg"
+  },
+  {
+    "revision": "f24a3859104f0c58fff6fd26537ab2b3",
+    "url": "/UpliftConference/static/media/instagram.f24a3859.svg"
+  },
+  {
+    "revision": "c3ee4c5b83eace43e68078930b7e165c",
+    "url": "/UpliftConference/static/media/quote-left.c3ee4c5b.svg"
+  },
+  {
+    "revision": "250e0c09952ffeb77a5076873494778a",
+    "url": "/UpliftConference/static/media/quote-right.250e0c09.svg"
+  },
+  {
+    "revision": "ea0d493856ebf7ad1767a365c367230f",
+    "url": "/UpliftConference/static/media/twitter.ea0d4938.svg"
+  },
+  {
+    "revision": "942e45e0af9fb30eaf0a486f098206cb",
+    "url": "/UpliftConference/static/media/upliftbanner.942e45e0.webp"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.baa1867fdb89d72266717e41beaedc9d.js"
+  "/UpliftConference/precache-manifest.2f74c2f049686f13fe513da3adbbf3be.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/UpliftConference/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
